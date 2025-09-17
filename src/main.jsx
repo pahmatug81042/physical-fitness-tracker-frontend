c734906d60f137f40ddc62cafc209c3c1acf0c65
pahmatug81042 <ptug81042@yahoo.com>
2025-09-17 21:25:06 +0000

    renamed the file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.js';
-import { ThemeProvider } from './context/THemeContext.js';
+import { ThemeProvider } from './context/ThemeContext.js';
 import './index.css';
 import App from './App.jsx';
 
