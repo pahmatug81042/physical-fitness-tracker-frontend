bc3c5caf0c6d6f46b8bb4f8ee32cb9049736c3d9
pahmatug81042 <ptug81042@yahoo.com>
2025-09-18 17:53:51 +0000

    fixed the imports in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext.js';
-import { ThemeProvider } from './context/ThemeContext.js';
+import { AuthProvider } from './context/AuthContext.jsx';
+import { ThemeProvider } from './context/ThemeContext.jsx';
 import './index.css';
 import App from './App.jsx';
 
