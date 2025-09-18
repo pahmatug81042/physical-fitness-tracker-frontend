70629704336c6d2c69ecddaa56c42dd52bcec3d4
pahmatug81042 <ptug81042@yahoo.com>
2025-09-18 23:04:55 +0000

    fixed the routes in workoutService to match the workout routes in the backend

--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -7,18 +7,18 @@ export const createWorkout = (payload) =>
     body: JSON.stringify(payload),
   });
 
-export const getWorkouts = () => apiClient("/workouts");
+export const getWorkouts = () => apiClient("/api/workouts/");
 
-export const getWorkoutById = (id) => apiClient(`/workouts/${id}`);
+export const getWorkoutById = (id) => apiClient(`/api/workouts/${id}`);
 
 export const updateWorkout = (id, payload) =>
-  apiClient(`/workouts/${id}`, {
+  apiClient(`/api/workouts/${id}`, {
     method: "PUT",
     body: JSON.stringify(payload),
   });
 
 export const deleteWorkout = (id) =>
-  apiClient(`/workouts/${id}`, { method: "DELETE" });
+  apiClient(`/api/workouts/${id}`, { method: "DELETE" });
 
 export const addExerciseToWorkout = (workoutId, exerciseData) =>
   updateWorkout(workoutId, { exercises: [exerciseData] });
